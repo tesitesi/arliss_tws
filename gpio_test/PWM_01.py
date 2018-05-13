1bc1900c1d1dc10a54b80ffdbc3fc8b1728c5cd5
temmafj <karaage417@softbank.ne.jp>
2018-05-13 17:45:17 +0900

    周波数変更

--- gpio_test/PWM_01.py
+++ gpio_test/PWM_01.py
@@ -15,14 +15,14 @@ pi.set_mode(gpio_pin0, pigpio.OUTPUT)
 pi.set_mode(gpio_pin1, pigpio.OUTPUT)
 
 # 飛行時duty比 duty_ratio
-pi.hardware_PWM(gpio_pin0, 2, int(duty_ratio * 1000000))
+pi.hardware_PWM(gpio_pin0, 8, int(duty_ratio * 1000000))
 
 time.sleep(10)
 
 #duty比を線形に0まで落とす
 while duty_ratio > 0:
     duty_ratio = duty_ratio - 0.01
-    pi.hardware_PWM(gpio_pin0, 2,  int(duty_ratio * 1000000))
+    pi.hardware_PWM(gpio_pin0, 8,  int(duty_ratio * 1000000))
     time.sleep(0.1)
 
 pi.set_mode(gpio_pin0, pigpio.INPUT)
