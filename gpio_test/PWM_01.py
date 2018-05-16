4ff2f2ddb5261ff04d0d91d60bb90945034e5914
temmafj <karaage417@softbank.ne.jp>
2018-05-16 16:32:59 +0900

    m

--- gpio_test/PWM_01.py
+++ gpio_test/PWM_01.py
@@ -12,16 +12,21 @@ duty_ratio = 0.5
 pi = pigpio.pi()
 pi.set_mode(gpio_pin0, pigpio.OUTPUT)
 
+i = 0
 # 飛行時duty比 duty_ratio
-pi.hardware_PWM(gpio_pin0, 8, int(duty_ratio * 1000000))
-
-time.sleep(10)
+while i < 100000:
+    pi.hardware_PWM(gpio_pin0, 8, int(duty_ratio * 1000000))
+    time.sleep(0.001)
+    i = i+ 1
 
 #duty比を線形に0まで落とす
+k = 0
 while duty_ratio > 0:
+    while k < 100: 
+        pi.hardware_PWM(gpio_pin0, 8,  int(duty_ratio * 1000000))
+        time.sleep(0.001)
+    k = 0
     duty_ratio = duty_ratio - 0.01
-    pi.hardware_PWM(gpio_pin0, 8,  int(duty_ratio * 1000000))
-    time.sleep(0.1)
 
 pi.set_mode(gpio_pin0, pigpio.INPUT)
 pi.stop()
