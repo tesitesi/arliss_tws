c139d42e6816922b00d8e4504df52be9a12fd716
temmafj <karaage417@softbank.ne.jp>
2018-06-15 17:48:00 +0900

    パラシュート

--- cds.ino
+++ cds.ino
@@ -1,16 +1,28 @@
-int cdsPin = XX;  //cdsピン番号
-int strartvalue = XX; //パラシュート展開判定用cds返り値
-int val;
+int cdsPin = XX;
+int pressurePin = YY;
+int nichromePin = ZZ;  //cdsピン番号
+int strartvalue = XX;
+int pressure_value = YY; //パラシュート展開判定用cds返り値
+int val1;
+int val2;
 
 void setup() {
   pinMode(cdsPin, INPUT);
+  pinMode(pressurePin, INPUT);
+  pinMode(nichromePin, OUTPUT);
 }
 
 void loop() {
-  val = analogRead(cdsPin);
-  if (val > startvalue) {
-    //パラシュート展開から切り離しまで
-    break;
+  val1 = analogRead(cdsPin);
+  if (val1 > startvalue) {
+    void while(1) {
+      val2 = analogRead(pressurePin);
+      if (val2 > pressure_value) {
+        digitalWrite(nichromePin, HIGH)
+        break;
+      }
+    }
+    while(1) {}
   }
   else {}
 }
