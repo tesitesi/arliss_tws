efb1b8337e9887c884d5ae944083fd1f59720036
Kenta Suda <sudaken0725@gmail.com>
2018-06-15 17:55:10 +0900

    edit cds

--- cds/cds.ino
+++ cds/cds.ino
@@ -1,8 +1,8 @@
-int cdsPin = XX;
-int pressurePin = YY;
-int nichromePin = ZZ;  //cdsピン番号
-int strartvalue = XX;
-int pressure_value = YY; //パラシュート展開判定用cds返り値
+int cdsPin = 0;
+int pressurePin = 0;
+int nichromePin = 0;  //cdsピン番号
+int startvalue = 0;
+int pressure_value = 0; //パラシュート展開判定用cds返り値
 int val1;
 int val2;
 
@@ -15,10 +15,10 @@ void setup() {
 void loop() {
   val1 = analogRead(cdsPin);
   if (val1 > startvalue) {
-    void while(1) {
+    while(1) {
       val2 = analogRead(pressurePin);
       if (val2 > pressure_value) {
-        digitalWrite(nichromePin, HIGH)
+        digitalWrite(nichromePin, HIGH);
         break;
       }
     }
