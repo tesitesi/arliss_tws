41d7c821da0dc2c9a91bef86ebf3672d8af577a4
temmafj <karaage417@softbank.ne.jp>
2018-08-16 01:48:49 +0900

    take_off

--- takeoff.ino/takeoff.ino.ino
+++ takeoff.ino/takeoff.ino.ino
@@ -5,9 +5,9 @@ int i=0;
 int k=0;
 int wait_time=2;
 int arming_time=3;
-int nose_up_time=5;
-int throttol_time=3;
-int ch[8]={500,500,0,500,500,500,500,0};
+int nose_up_time=2;
+int throttol_time=0;
+int ch[8]={500,900,0,500,500,500,500,0};
 
 void setup() {
   Serial.begin(9600);
@@ -34,7 +34,7 @@ void loop() {
       /*ARMING*/
       case 1:
         Serial.println("case1 arm");
-        ch[3]=1000;//rudder
+        ch[3]=1100;//rudder
         while (i<50*arming_time) {
           PPM(ch);
           i++;
@@ -44,7 +44,7 @@ void loop() {
         break;
       case 2:
         Serial.println("case2 elevator only");
-        ch[1]=667; //elevator
+        ch[1]=1100; //elevator
         ch[3]=500;
         ch[7]=1000; //fight mode==auto
         while (i<50*nose_up_time) {
@@ -57,20 +57,21 @@ void loop() {
       case 3:
         Serial.println("case3 elevator&throttle");
         ch[2]=300; //throttol
-        while (k<50*throttol_time) {
+        while (i<50*throttol_time) {
           PPM(ch);
-          k++;
+          i++;
         }
         i = 0;
         var ++;
         break;
       case 4:
         Serial.println("case4 fly");
-        ch[1]=370; //elevator
+        ch[1]=100; //elevator
         ch[2]=0; //throttol
+        ch[7]=1000;
         while(1){
           PPM(ch);
-          k ++;
+          i ++;
         }
         break;
   }
